67cce1cd515204fba541f5386b1938669d8e1dba
brianBoolean88 <brian.is.true.false@gmail.com>
2025-06-25 00:43:26 -0700

    NavBar for Mobile

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -7,17 +7,17 @@ import NavAnchor from './NavAnchor'
 const NavBar = () => {
     return (
         <nav className="fixed top-0 left-0 w-full h-20 bg-[#222] bg-opacity-80 z-[200] shadow-lg flex items-center px-8">
-            <Link href={"/"} className='text-3xl text-white font-semibold flex items-center mr-10'>
+            <Link href={"/"} className='text-xl md:text-2xl lg:text-3xl text-white font-semibold flex items-center mr-10'>
                 <Image
                     src="/images/nav_img.png"
                     alt="Logo"
                     width={48}
                     height={48}
-                    className="w-12 h-12 object-contain mr-2"
+                    className="w-6 h-6 md:w-12 md:h-12 object-contain mr-2"
                 />
                 Brian Wang
             </Link>
-            <ul className="flex flex-row space-x-10 ml-auto">
+            <ul className="flex flex-row space-x-2 lg:space-x-10 ml-auto">
                 <NavLink href={"/"} title={"Home"} />
                 <NavAnchor href={"/#about"} title={"About"} />
                 <NavLink href={"/projects"} title={"Projects"} />
--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, title, onClick }) => {
     return (
         <Link href={href}
             onClick={onClick}
-        className="bg-[#AEC8A4] block py-2 pl-3 pr-4 mr-4 text-white text-lg font-semibold hover:text-[#E7EFC7] hover:bg-[#8A784E] rounded transition-colors duration-300 ease-in-out">
+        className="bg-[#AEC8A4] block py-2 pl-1 pr-1 sm:pl-2 sm:pr-2 md:pl-4 md:pr-4 mr-4 text-white text-lg font-semibold hover:text-[#E7EFC7] hover:bg-[#8A784E] rounded transition-colors duration-300 ease-in-out">
             {title}
         </Link>
     );
